0d7bee1c04b208803dad6b75ae1ba7d3d3e2d18b
Hagee <hagee.ktr@outlook.com>
2025-05-28 15:16:12 +0300

    [Refactor]: Generate property method to the email

--- lib/models/user.py
+++ lib/models/user.py
@@ -1,9 +1,10 @@
 import datetime
 from sqlalchemy import UniqueConstraint, Column, Integer, String, DateTime
 from lib.models import Base
+import re
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     __table_args__ = (
         UniqueConstraint(
             'email', 
@@ -13,8 +14,21 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    email = Column(String(55))
+    _email = Column(String(55))
     signup_date = Column(DateTime(), default=datetime.datetime.now)
+
+    def __init__(self, email=None):
+        self._email = email
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            raise ValueError('Invalid email address')
+        self._email = email
 
     def __repr__(self):
         return f"<User(id={self.id}, Name='{self.name}', Email Address='{self.email}', Sign Up Date='{self.signup_date}')>"
