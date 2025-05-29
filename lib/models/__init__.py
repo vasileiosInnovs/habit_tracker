41516b415fb2517c94ab41d0d9ee3810e022698a
Hagee <hagee.ktr@outlook.com>
2025-05-29 10:58:30 +0300

    [Fix]: Correct bug

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,2 +1,4 @@
 from .base import Base
 from .user import User
+from .log import Log
+from .habit import Habit
---
+++ main.py
@@ -0,0 +1,4 @@
+from lib.cli.menu import main_menu
+
+if __name__ == '__main__':
+    main_menu()
